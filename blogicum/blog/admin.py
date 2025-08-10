7617475f65a8d5300de17d94257889c330be030c
Ekaterina <suhowa.ekaterina2013@yandex.ru>
2025-08-10 21:13:42 +0400

    add models

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Post, Location
+
+admin.site.register(Post)
+admin.site.register(Location)
+admin.site.register(Category)
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,47 +1,8 @@
 from django.shortcuts import render
+from blog.models import Post, Category
+from django.http import Http404
+from django.utils import timezone
 
-posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
-    },
-]
 
 empty_post = {
     'id': None,
@@ -54,20 +15,52 @@ empty_post = {
 
 def index(request):
     template = 'blog/index.html'
-    context = {'posts': posts[::-1]}
+    post_list = (
+        Post.objects
+        .filter(is_published__exact=True,
+                category__is_published__exact=True,
+                created_at__lte=timezone.now())
+        .order_by('-created_at')
+        .all()[:5]
+    )
+    context = {'post_list': post_list}
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
-    ids = [post['id'] for post in posts]
-    if id in ids:
-        context = {'post': posts[id]}
-        return render(request, template, context)
-    return render(request, template_name='blog/index.html')
+
+    try:
+        post = Post.objects.get(pk=id, 
+                                is_published=True,
+                                category__is_published=True,
+                                created_at__lte=timezone.now())
+
+    except Post.DoesNotExist:
+        raise Http404()
+
+    context = {'post': post}
+    return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {'category': category_slug}
+    try:
+        category = (Category.objects
+                   .get(slug=category_slug, 
+                        is_published=True))
+
+    except Category.DoesNotExist:
+        raise Http404()
+    
+    post_list = (
+        Post.objects
+        .filter(is_published__exact=True,
+                category__pk__exact=category.pk,
+                created_at__lte=timezone.now())
+        .all()
+    )
+
+    context = {'category': category,
+               'post_list': post_list}
     return render(request, template, context)
