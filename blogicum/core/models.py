2ce1e1e9465b35452f4ba868b9a9d1a3417bae63
Ekaterina <suhowa.ekaterina2013@yandex.ru>
2025-08-10 21:27:48 +0400

    f

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добавляет флаг is_published."""
- 
+
     is_published = models.BooleanField('Опубликовано',
                                        default=True,
                                        help_text="Снимите галочку, "
@@ -11,7 +11,7 @@ class PublishedModel(models.Model):
 
     class Meta:
         abstract = True
-     
+
 
 class CreatedModel(models.Model):
     """Абстрактная модель. Добавляет флаг is_published."""
